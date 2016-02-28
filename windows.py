278fea7690199449dadf1c504cef8a4b8adb6973
Kopiks <mr.reneg8@gmail.com>
2016-02-28 19:33:06 +0100

    cleaned up

--- windows.py
+++ windows.py
@@ -6,38 +6,27 @@ for n in strand:
                 try:
                         temp = []
                         score = 0
-                        print i
-                        if seq[i] == n[0]:
-                                print "".join(seq[i:i+3])            
+                        if seq[i] == n[0]:     
                                 if seq[i+2] == n[0]:
                                         leng = 3
                                         tetrs = 1
                                         if "".join(seq[i:i+3]) != n[1]:
                                                 score += 1
-                                        print score
                                         temp.append(seq[i:i+4])
-                                        print temp
                                         flag = 4
                                         while len("".join(temp))<98 and tetrs < 4:
-                                                print 'check'
                                                 if seq[i+flag] == n[0]:
-                                                        print "znalazlem G"
                                                         if seq[i+flag+2] == n[0]:
                                                                 if "".join(seq[i+flag:i+flag+3]) != n[1]:
                                                                         score += 1
-                                                                        print score
                                                                 if tetrs != 3:
                                                                         temp.append(seq[i+flag:i+flag+4])
                                                                         tetrs += 1
                                                                 else:
                                                                         temp.append(seq[i+flag:i+flag+3])
-                                                                        print temp
-                                                                        print 'LALA'
                                                                         g_counter = 0
                                                                         tetrs += 1
                                                                         while seq[i+flag+3+g_counter] == n[0]:
-                                                                                print temp
-                                                                                print 'LALALA'
                                                                                 temp.append(seq[i+flag+3+g_counter])
                                                                                 g_counter += 1
                                                                      
@@ -52,20 +41,14 @@ for n in strand:
                                                             
                                                         temp.append(seq[i+flag])
                                                         flag += 1
-                                                print temp
                                         if tetrs == 4:
                                                 tab.append(["".join(temp), score, i+1, i+(len("".join(temp))), n[2]])
-                                        print tab
                 except IndexError:
-                        print "IERROR"
                         if seq[len(seq)-1]==n[0]:
                                # temp.append("G")
-                                print "APPENDING"
-                                print temp
                                 if tetrs == 4:
                                         tab.append(["".join(temp),score,i+1,i+(len("".join(temp))), n[2]])
                         else:
-                           print "PASSING/BREAKING"
                            break
 print "LENSEQ: %d"%(len(seq))
 print "FINAL:"
