438c4903bc3846ecc0fe0f30efe3f1031ac1fe32
Kopi_wydzial <kopiamu@gmail.com>
2016-12-09 13:03:06 +0100

    not much more

--- nt_switcher.py
+++ nt_switcher.py
@@ -1,6 +1,12 @@
-var_in=open("filtered_prom_s_var_raw.vcf", "r");
-	for line in var_in:
-		if line.startswith("#"):
-			line=line.split("\t")
-			info=line[7].split(";")
+var_in=open("filtered_prom_s_var_raw.vcf", "r")
+for line in var_in:
+	if not line.startswith("#"):
+		line=line.split("\t")[7]
+		info=line.split(";")
+		if info[0]=="INDEL": info[0]="INDEL=1"
+		print info
+		dic=dict(item.split("=") for item in info)
+		print dic["AC"]
+		if dic["AC"]==2:
+			
 			
